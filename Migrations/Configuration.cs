f219616552a99dd279773a71dcae4b78fa7bf637
Mert <80112852+merturun@users.noreply.github.com>
2022-06-18 07:30:07 +0300

    asd
    asd

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -18,6 +18,7 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            
         }
     }
 }
--- Models/Group.cs
+++ Models/Group.cs
@@ -13,7 +13,8 @@ namespace MIM.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public Organization Organization { get; set; }
-        public ICollection<Grant> Grants { get; set; }     
+        public ICollection<Grant> Grants { get; set; }
+        public ICollection<User> Users { get; set; }
 
     }
 }
