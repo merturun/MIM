66608a6f8bb4c5a3f8ed4638cabae21320d21045
Mert <80112852+merturun@users.noreply.github.com>
2022-06-18 06:37:56 +0300

    Modeller
    Modeller

---
+++ Models/Module.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MIM.Models
+{
+    public class Module
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -10,7 +10,17 @@ namespace MIM.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public Organization Organization { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
+        public string Username { get; set; }
+        public Title Title { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ActivationCode { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime BornDate { get; set; }
+        public bool SuperAdmin { get; set; }
     }
 }
